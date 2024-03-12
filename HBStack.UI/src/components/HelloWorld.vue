dbbc7906ba5c5875b434304672384eaa279480ce
KonDzik <k.szpytma3@gmail.com>
2024-03-12 22:42:29 +0100

    Define counter store

--- HBStack.UI/src/components/HelloWorld.vue
+++ HBStack.UI/src/components/HelloWorld.vue
@@ -1,18 +1,21 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
+import { useCounterStore } from "../store/counterStore";
+
+const store = useCounterStore();
 
 defineProps({
   msg: String,
-})
-
-const count = ref(0)
+});
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="store.increment">
+      count is {{ store.count }}
+    </button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
