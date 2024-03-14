08cc15729ecc59da459a784f9db20235ff376ea7
KonDzik <k.szpytma3@gmail.com>
2024-03-14 22:41:24 +0100

    Add vue-router dependency

--- HBStack.UI/src/App.vue
+++ HBStack.UI/src/App.vue
@@ -1,17 +1,10 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup lang="ts">
+import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div></div>
+  <RouterView />
 </template>
 
 <style scoped>
@@ -28,3 +21,4 @@ import HelloWorld from './components/HelloWorld.vue'
   filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
+./components/Home/HomeView.vue
---
+++ HBStack.UI/src/components/Home/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <h1>Home</h1>
+  <p>
+    Edit
+    <code>components/Home/Home.vue</code> to test HMR
+  </p>
+  <p>
+    Check out
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite</a>
+    and
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3</a>
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
