b017e72abdcbd30f22572542d4281bdb530d2dd5
KonDzik <k.szpytma3@gmail.com>
2024-03-18 17:21:18 +0100

    Update MainLayout.vue and remove vue.svg

--- HBStack.UI/src/layouts/MainLayout.vue
+++ HBStack.UI/src/layouts/MainLayout.vue
@@ -8,14 +8,21 @@ const responsivenessStore = useResponsivenessStore();
   <v-row> top </v-row>
   <v-row class="spacer"></v-row>
   <v-row>
+    <v-col cols="3"> </v-col>
+    <v-col cols="4">
+      <v-row class="d-flex align-center">
+        <v-col cols="3">
+          <img src="/logo.png" class="logo" alt="logo" width="144" />
+        </v-col>
+        <v-col> hbstack </v-col>
+      </v-row>
+    </v-col>
+    <v-col cols="5"> </v-col>
+
     <v-col cols="4"> </v-col>
     <v-col cols="4">
-      hbstack
-
-      <v-row>
-        <v-col v-for="n in 10" :key="n" cols="12">
-          category title lorem ipsum des emore asater
-        </v-col>
+      <v-row v-for="n in 13" :key="n">
+        <v-col cols="12"> category title lorem ipsum des emore asater </v-col>
       </v-row>
     </v-col>
     <v-col cols="4"> </v-col>
